ae0cb94d902d0d68e3d2ce0e4e961a713fbd4ed2
alessio1210 <alessio8112@gmail.com>
2026-05-01 12:27:48 +0200

    created first yaml file to get it in the terminal

--- forge/main.py
+++ forge/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import typer
+from rich.console import Console
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = typer.Typer()
+console = Console()
 
+@app.command()
+def main():
+    print("test")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
